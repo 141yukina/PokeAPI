bde38e7d4123d76c3e7d4072070db1ec0420e368
USENGROUP\ykn-ishi <ykn-ishi@usen-next.jp>
2023-06-07 11:08:38 +0900

    キャッシュをゲットだぜ！

--- get_pokemon_data.php
+++ get_pokemon_data.php
@@ -1,78 +1,82 @@
 <?php
-function getCacheContents($url, $cachePath, $cacheLimit = 86400) {
-  if(file_exists($cachePath) && filemtime($cachePath) + $cacheLimit > time()) {
-    // キャッシュ有効期間内なのでキャッシュの内容を返す
-    return file_get_contents($cachePath);
-  } else {
-    // キャッシュがないか、期限切れなので取得しなおす
-    $data = file_get_contents($url);
-    file_put_contents($cachePath, $data, LOCK_EX); // キャッシュに保存
-    return $data;
-  }
-}
-
-
 $a = 1;
-$b = 10*$a;
+$b = 10 * $a;
 
-if(isset($_POST['a'])){
+if (isset($_POST['a'])) {
     $a = $_POST['a'];
 }
-if(isset($_POST['plus'])){
+if (isset($_POST['plus'])) {
     $a++;
-    $b = 10*$a;
+    $b = 10 * $a;
 }
 
-if(isset($_POST['minus'])){
-    $b = 9*($a- 1) + 1;
-    $a = $a - 1 ;
+if (isset($_POST['minus'])) {
+    $b = 9 * ($a - 1) + 1;
+    $a = $a - 1;
 }
 
+// キャッシュディレクトリを作成
+if (!is_dir('cache')){
+    mkdir('cache');
+}
 
-
-    for($i=$b;$i<($b + 10);$i++){
-
+// ポケモンデータのキャッシュをゲットだぜ
+$pokemonData = array();
+for ($i = $b; $i < ($b + 10); $i++) {
+    $cacheFile = "cache/$i.json";
+    if (file_exists($cacheFile)) {
+        $response = file_get_contents($cacheFile);
+        $value = json_decode($response, true);
+    } else {
         $url = "https://pokeapi.co/api/v2/pokemon/$i/";
         $response = file_get_contents($url);
-        $value= json_decode($response, true);
-        
-        
-        
-        $name = $value['name'];
-        $image = $value['sprites']['front_default'];
-        $height = $value['height'];
-        $weight = $value['weight'];
-        $type[$i] = $value['types'];
-        
-        $type1 = $type[$i][0]['type']['name'];
-        
-        if(isset($type[$i][1])){
-        $type2 = $type[$i][1]['type']['name'];
-        }else{$type2 = "";}
-        
-        $conf = fopen("pokemon.tmpl", "r");
-        $size = filesize("pokemon.tmpl");
-        $tmpl = fread($conf, $size);
-        fclose($conf);
-        
-        $tmpl = str_replace("!name!", $name, $tmpl);
-        $tmpl = str_replace("!image!", $image, $tmpl);
-        $tmpl = str_replace("!height!", $height, $tmpl);
-        $tmpl = str_replace("!weight!", $weight, $tmpl);
-        if($type2 !== ""){
-        $tmpl = str_replace("!type!",$type1."/".$type2,$tmpl);
-        }else{$tmpl = str_replace("!type!",$type1,$tmpl);
-        }
-        
-        echo $tmpl;
- 
-        
-        }
+        $value = json_decode($response, true);
+        $cachedValue = json_encode($value);
+        file_put_contents($cacheFile, $cachedValue);
+    }
+    $pokemonData[$i] = $value;
+}
 
+// ポケモンデータを表示
+foreach ($pokemonData as $i => $value) {
+    $name = $value['name'];
+    $image = $value['sprites']['front_default'];
+    $height = $value['height'];
+    $weight = $value['weight'];
+    $type = $value['types'];
 
-    
+    $type1 = $type[0]['type']['name'];
+
+    if (isset($type[1])) {
+        $type2 = $type[1]['type']['name'];
+    } else {
+        $type2 = "";
+    }
+
+    $conf = fopen("pokemon.tmpl", "r");
+    $size = filesize("pokemon.tmpl");
+    $tmpl = fread($conf, $size);
+    fclose($conf);
+
+    $tmpl = str_replace("!name!", $name, $tmpl);
+    $tmpl = str_replace("!image!", $image, $tmpl);
+    $tmpl = str_replace("!height!", $height, $tmpl);
+    $tmpl = str_replace("!weight!", $weight, $tmpl);
+    if ($type2 !== "") {
+        $tmpl = str_replace("!type!", $type1 . "/" . $type2, $tmpl);
+    } else {
+        $tmpl = str_replace("!type!", $type1, $tmpl);
+    }
+
+    echo $tmpl;
+    echo '<form action="get_pokemon_data.php" method="POST">
+        <input type="submit" name="details" value="details">
+        </form>';
+}
+
 ?>
-    <!DOCTYPE html>
+
+<!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -95,5 +99,4 @@ if(isset($_POST['minus'])){
     
  </form>
  <?=$a?>
-
 
